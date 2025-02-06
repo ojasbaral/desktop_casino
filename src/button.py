3342d4bae603d642dcaf3e8316645c27c77c92ac
Ojas Baral <ojas.baral@gmail.com>
2025-02-06 16:37:36 -0700

    added menu functionality

--- src/button.py
+++ src/button.py
@@ -9,6 +9,7 @@ class Button:
         self.width = width
         self.height = height
         self.text = text
+        self.font = pygame.font.Font('./static/fonts/Born2bSportyFS.otf', 50)
 
     def draw(self, win, outline=None):
         if outline:
@@ -17,14 +18,13 @@ class Button:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
 
         if self.text != '':
-            font = pygame.font.SysFont('comicsans', 60)
-            text = font.render(self.text, 1, (0, 0, 0))
+            text = self.font.render(self.text, 1, 'white')
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isHovering(self, pos):
         # Pos is the mouse position or a tuple of (x, y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if pos[0] > self.x and pos[0] < (self.x + self.width):
+            if pos[1] > self.y and pos[1] < (self.y + self.height):
                 return True
             
         return False
--- src/run.py
+++ src/run.py
@@ -1,11 +1,36 @@
 import pygame
+from button import Button
 
 FELT_COLOR = (53, 101, 77)
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+GAME_MODES = ["Blackjack", "Quit"]
 
-def centered_coords(obj: pygame.Surface):
-    return ((SCREEN_WIDTH - obj.get_width()) // 2, (SCREEN_HEIGHT - obj.get_height()) // 2)
+def display_modes(game_modes, screen):
+    # calculate where each button should be for it to be centered
+    PADDING = 20
+    WIDTH, HEIGHT = 300, 60
+    X = screen.get_width() // 2
+    # need to calc the x, y for each button
+    half = len(game_modes) // 2
+    y_list = [((i-half) * (PADDING + HEIGHT)) + (screen.get_height() // 2) for i in range(len(game_modes))]
+    buttons = []
+    for mode, y in zip(game_modes, y_list):
+        button = Button(FELT_COLOR, X - (WIDTH // 2), y, WIDTH, HEIGHT, mode)
+        buttons.append((mode, button))
+
+    return buttons
+
+def handle_click(buttons):
+    for mode, button in buttons:
+        if button.isHovering(pygame.mouse.get_pos()):
+            match mode:
+                case "Blackjack":
+                    print(mode)
+                    return True
+                case "Quit":
+                    return False
+
 
 def main():
     # pygame setup
@@ -14,18 +39,26 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.set_caption("Menu")
     running = True
-    font = pygame.font.Font(None, 60)
+    font = pygame.font.Font('./static/fonts/Born2bSportyFS.otf', 60)
+    quit_index = len(GAME_MODES) - 1
+    buttons = display_modes(GAME_MODES, screen)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                running = handle_click(buttons)
+
         screen.fill(FELT_COLOR)
 
         # starting menu text
         menu_text = font.render("Welcome to my Casino!", True, 'white')
-        screen.blit(menu_text, centered_coords(menu_text))
+        screen.blit(menu_text, ((SCREEN_WIDTH - menu_text.get_width()) // 2, 30))
+
+        for button in buttons:
+            button[1].draw(screen, "white")
 
         pygame.display.flip()
         
